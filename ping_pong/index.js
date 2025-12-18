2fbc3f8ad9f82d96a921941f8123063a3abca77a
calculator <hainguyen240195@gmail.com>
2025-12-18 18:40:49 +0200

    Add Chapter 4

---
+++ ping_pong/index.js
@@ -0,0 +1,8 @@
+app.get('/healthz', async (req, res) => {
+  try {
+    await pool.query('SELECT 1'); // Thử gửi một câu lệnh truy vấn đơn giản
+    res.status(200).send('OK');
+  } catch (err) {
+    res.status(500).send('Not Ready');
+  }
+});
